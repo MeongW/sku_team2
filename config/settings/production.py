0e51b87bae6044f1dd3899ad621bf77f714e860e
MeongW <easymiracle@skuniv.ac.kr>
2023-07-30 17:19:13 +0900

    sku_team2

--- config/settings/production.py
+++ config/settings/production.py
@@ -38,4 +38,4 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
