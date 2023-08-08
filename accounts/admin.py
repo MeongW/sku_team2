140659a5fccf78e6974cd6c8c9c277e2d9123dab
MeongW <125392666+MeongW@users.noreply.github.com>
2023-08-08 17:11:50 +0900

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-
-CustomUser = get_user_model()
-#admin.site.register(CustomUser)
-
-@admin.register(CustomUser)
-class CustomUserModelAdmin(admin.ModelAdmin):
-    pass
