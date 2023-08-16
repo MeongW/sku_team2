591be23b96187387fe3d104f5809d191773abb18
MeongW <125392666+MeongW@users.noreply.github.com>
2023-08-17 03:20:54 +0900

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -260,7 +260,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 # cors setting
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
     'http://127.0.0.1:8080',
