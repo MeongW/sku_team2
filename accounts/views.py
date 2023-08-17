3f9d62cc04834c98af730ce1ff7008d2ffddf7c3
MeongW <125392666+MeongW@users.noreply.github.com>
2023-08-18 05:04:38 +0900

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -184,7 +184,7 @@ BASE_URL = settings.BASE_URL
 
 KAKAO_CALLBACK_URI = f"{BASE_URL}/api/accounts/social/kakao/callback"
 
-@api_view(["POST"])
+@api_view(["GET"])
 @permission_classes([AllowAny])
 def kakao_callback(request):
     rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
