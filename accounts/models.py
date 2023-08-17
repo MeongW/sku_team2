6e066b9b4afa45df3e8790dccfb47b9cb72290b4
MeongW <125392666+MeongW@users.noreply.github.com>
2023-08-18 02:54:19 +0900

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -87,7 +87,6 @@ class SMSAuthentication(models.Model):
         return base64.b64encode(hmac.new(secret_key, message, digestmod=hashlib.sha256).digest())
     
     def send_sms(self):
-        logger.info("test-info")​​
         
         timestamp = str(int(time.time() * 1000))
         access_key = getattr(
